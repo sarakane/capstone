b42d2bf990d874b19b334e7cbe8e54ba0bd8e331
Sara Kane <kane.sarak@gmail.com>
2020-10-05 12:03:43 -0700

    Add redux and binding libraries for firestore

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import LibraryControl from './LibraryControl';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <LibraryControl />
     </React.Fragment>
   );
 }
