ef48d6af5571001a2663f618cfe63ae41916a5e1
Sara Kane <kane.sarak@gmail.com>
2020-10-05 17:01:59 -0700

    Add functionality to delete a library

--- src/components/libraries/LibraryDetail.js
+++ src/components/libraries/LibraryDetail.js
@@ -2,23 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function LibraryDetail(props) {
-  const { library } = props;
+  const { library, onClickingDelete } = props;
 
   return (
     <React.Fragment>
       <h1>Library</h1>
       <h2>{library.libraryName}</h2>
       <button>Edit</button>
-      <button>Delete</button>
+      <button onClick={() => onClickingDelete(library.id)}>Delete</button>
       <hr />
       <h3>Sections</h3>
       <button>New Section</button>
+      <hr />
     </React.Fragment>
   );
 }
 
 LibraryDetail.propTypes = {
   library: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default LibraryDetail;
