e178db6e7b31fb2f54074e4f6067a74af898dce0
Sara Kane <7584520+sarakane@users.noreply.github.com>
2020-12-15 13:57:21 -0800

    Add routes in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Navigation from './components/Navigation';
-import ProjectControl from './components/ProjectControl';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as route from './constants/routes';
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Navigation />
       <div className="container">
-        <ProjectControl />
+        <Switch>
+          <Route exact path={route.LANDING}></Route>
+          <Route path={route.HOME}></Route>
+          <Route path={route.ABOUT}></Route> 
+          <Route path={route.FORGOT_PASSWORD}></Route>
+          <Route path={route.ACCOUNT}></Route>
+          <Route path={route.SIGN_IN}></Route>
+          <Route path={route.SIGN_UP}></Route>
+          <Route path={route.FORGOT_PASSWORD}></Route>
+        </Switch>
       </div>
-    </React.Fragment>
+    </Router>
   );
 }
 
