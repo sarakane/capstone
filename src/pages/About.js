04ae2bd8ccf49676c504733096b5b8f623b223af
Sara Kane <7584520+sarakane@users.noreply.github.com>
2020-12-15 13:57:43 -0800

    Add pages

---
+++ src/pages/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return (<><h2>About</h2><p></p></>);
+};
+
+export default About;
---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+
+const Landing = () => {
+  return (
+    <>
+      <h1>Welcome to Resourcey!</h1>
+      <p>Share your learning resources with your classmates</p>
+    </>
+  );
+  
+  ;
+};
+
+export {Landing};
