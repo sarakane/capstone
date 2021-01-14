3d302e50c78a4f3b1f0f8bfb1a0ebc6bb499179a
Sara Kane <7584520+sarakane@users.noreply.github.com>
2021-01-13 23:42:11 -0800

    Make sure state is correct in LibraryDetail even if page is reloaded

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as route from './constants/routes';
-import Navigation from "./components/Navigation";
+import Navigation from './components/Navigation';
 import Account from './pages/Account';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgotPassword from './pages/ForgotPassword';
-import LibraryDetail from "./components/libraries/LibraryDetail";
-
+import LibraryDetail from './components/libraries/LibraryDetail';
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <div className="container">
-        <Switch>
-          <Route exact path={route.LANDING} component={Landing} />
-          <Route path={route.HOME} component={Home} />
-          <Route path={route.FORGOT_PASSWORD} component={ForgotPassword} />
-          <Route path={route.SIGN_IN} component={SignIn} />
-          <Route path={route.SIGN_UP} component={SignUp} />
-          <Route path={route.ACCOUNT} component={Account} />
-          <Route path={route.LIBRARY_PAGE} component={LibraryDetail} />
-        </Switch>
+      <div className='container'>
+        <Route exact path={route.LANDING} component={Landing} />
+        <Route path={route.HOME} component={Home} />
+        <Route path={route.FORGOT_PASSWORD} component={ForgotPassword} />
+        <Route path={route.SIGN_IN} component={SignIn} />
+        <Route path={route.SIGN_UP} component={SignUp} />
+        <Route path={route.ACCOUNT} component={Account} />
+        <Route path={route.LIBRARY_PAGE} component={LibraryDetail} />
       </div>
     </Router>
   );
--- src/components/libraries/LibraryDetail.js
+++ src/components/libraries/LibraryDetail.js
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import SectionList from '../sections/SectionList'
+import SectionList from '../sections/SectionList';
 import NewSectionForm from '../sections/NewSectionForm';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { isLoaded, useFirestoreConnect } from 'react-redux-firebase';
+import { selectLibrary } from './libraryActions';
 
 const editButtonStyle = {
-  marginRight: '5px'
+  marginRight: '5px',
 };
 
 function LibraryDetail({ match }) {
   const [addingSection, setAddingSection] = useState(false);
-  const library = useSelector(state => state.firestore.ordered.libraries.find(e => e.id === match.params.id));
-  console.log(match);
+  const dispatch = useDispatch();
+  dispatch(selectLibrary(match.params.id));
+  useFirestoreConnect([{ collection: 'libraries', doc: match.params.id }]);
+  const library = useSelector(
+    ({ firestore: { data } }) =>
+      data.libraries && data.libraries[match.params.id]
+  );
 
   const toggleNewSectionForm = () => {
     setAddingSection(!addingSection);
-  }
+  };
 
-  return (
-    <React.Fragment>
-      <h1>Library</h1>
-      <h2>{library.libraryName}</h2>
-      {/* <button onClick={onClickingEdit} className="btn deep-purple darken-4" style={editButtonStyle}>Edit</button>
-      <button onClick={() => onClickingDelete(library.id)} className="btn deep-purple darken-4">Delete</button>
-      <hr />
-      <h3>Sections</h3>
-      <SectionList libraryId={library.id} whenSectionClicked={whenSectionClicked}/>
-      {addingSection && <NewSectionForm onNewSectionCreation={toggleNewSectionForm} libraryId={library.id}/>}
-      <button onClick={toggleNewSectionForm} className="btn blue-grey lighten-1">{addingSection ? "Cancel": "New Section"}</button>
-      <hr /> */}
-    </React.Fragment>
-  );
+  if (isLoaded(library)) {
+    return (
+      <React.Fragment>
+        <h1>Library</h1>
+        <h2>{library.libraryName}</h2>
+        {/* <button onClick={onClickingEdit} className="btn deep-purple darken-4" style={editButtonStyle}>Edit</button>
+                <button onClick={() => onClickingDelete(library.id)} className="btn deep-purple darken-4">Delete</button>
+                <hr />
+                <h3>Sections</h3>
+                <SectionList libraryId={library.id} whenSectionClicked={whenSectionClicked}/>
+                {addingSection && <NewSectionForm onNewSectionCreation={toggleNewSectionForm} libraryId={library.id}/>}
+                <button onClick={toggleNewSectionForm} className="btn blue-grey lighten-1">{addingSection ? "Cancel": "New Section"}</button>
+                <hr /> */}
+      </React.Fragment>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
 LibraryDetail.propTypes = {
