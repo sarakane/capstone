d92c690bd3817e0b0e7dca0a4425ab47a7752a45
Sara Kane <7584520+sarakane@users.noreply.github.com>
2021-01-15 12:49:25 -0800

    Update SectionDetails

--- src/components/sections/SectionDetails.js
+++ src/components/sections/SectionDetails.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import EditSectionForm from './EditSectionForm';
 import ResourceList from '../resources/ResourceList';
-import { useDispatch, useSelector } from 'react-redux';
-import { isLoaded, useFirestore, useFirestoreConnect } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
+import {
+  isLoaded,
+  useFirestore,
+  useFirestoreConnect,
+} from 'react-redux-firebase';
 import NewResourceForm from '../resources/NewResourceForm';
 
 function SectionDetails({ match, history }) {
@@ -11,52 +15,97 @@ function SectionDetails({ match, history }) {
   const firestore = useFirestore();
 
   useFirestoreConnect([
-    { collection: 'libraries',  doc: match.params.id, subcollections: [{collection: 'sections', doc: match.params.id2}], storeAs: 'sections'},
+    {
+      collection: 'libraries',
+      doc: match.params.id,
+      subcollections: [{ collection: 'sections', doc: match.params.id2 }],
+      storeAs: 'sections',
+    },
   ]);
-  
-  const section = useSelector(state => state.firestore.ordered.sections && state.firestore.ordered.sections.find(e => e.id === match.params.id2));
-  const sectionResources = useSelector(state => state.firestore.ordered.resources);
+
+  const section = useSelector(
+    (state) =>
+      state.firestore.ordered.sections &&
+      state.firestore.ordered.sections.find((e) => e.id === match.params.id2)
+  );
+  const sectionResources = useSelector(
+    (state) => state.firestore.ordered.resources
+  );
 
   function handleDeletingSection(sectionId) {
-    sectionResources.forEach(resource => firestore.delete({collection: 'libraries', doc: match.params.id, subcollections: [{collection: 'resources', doc: resource.id}]}));
-    firestore.delete({collection: 'libraries',  doc: match.params.id, subcollections: [{collection: 'sections', doc: sectionId}]});
+    sectionResources.forEach((resource) =>
+      firestore.delete({
+        collection: 'libraries',
+        doc: match.params.id,
+        subcollections: [{ collection: 'resources', doc: resource.id }],
+      })
+    );
+    firestore.delete({
+      collection: 'libraries',
+      doc: match.params.id,
+      subcollections: [{ collection: 'sections', doc: sectionId }],
+    });
     history.push(`/library/${match.params.id}`);
   }
 
-
-  if(isLoaded(section)) {
+  if (isLoaded(section)) {
     return (
       <>
         <h1>Section</h1>
-        {editing && <EditSectionForm section={section} setEditing={setEditing} />}
+        {editing && (
+          <EditSectionForm section={section} setEditing={setEditing} />
+        )}
         {!editing && <h2>{section.sectionName}</h2>}
-        <button onClick={() => setEditing(state => !state)} style={{marginRight: '5px'}} className="btn deep-purple darken-4">{!editing? "Edit": "Cancel"}</button>
-        <button onClick={() => handleDeletingSection(section.id)} className="btn deep-purple darken-4">Delete</button>
+        <button
+          onClick={() => setEditing((state) => !state)}
+          style={{ marginRight: '5px' }}
+          className='btn deep-purple darken-4'
+        >
+          {!editing ? 'Edit' : 'Cancel'}
+        </button>
+        <button
+          onClick={() => handleDeletingSection(section.id)}
+          className='btn deep-purple darken-4'
+        >
+          Delete
+        </button>
         <hr />
         <h3>Resources</h3>
         <ResourceList sectionId={section.id} />
-        {!creatingNewResource && <button onClick={() => setCreatingNewResource(state => !state)} className="btn blue-grey lighten-1">New Resource</button>}
-        {creatingNewResource && <NewResourceForm sectionId={section.id} setCreatingNewResource={setCreatingNewResource} />}
+        {!creatingNewResource && (
+          <button
+            onClick={() => setCreatingNewResource((state) => !state)}
+            className='btn blue-grey lighten-1'
+          >
+            New Resource
+          </button>
+        )}
+        {creatingNewResource && (
+          <NewResourceForm
+            sectionId={section.id}
+            setCreatingNewResource={setCreatingNewResource}
+          />
+        )}
       </>
     );
   } else {
     return (
       <>
-        <div className="preloader-wrapper small active">
-          <div className="spinner-layer spinner-green">
-            <div className="circle-clipper left">
-              <div className="circle"></div>
+        <div className='preloader-wrapper small active'>
+          <div className='spinner-layer spinner-green'>
+            <div className='circle-clipper left'>
+              <div className='circle'></div>
             </div>
-            <div className="gap-patch">
-              <div className="circle"></div>
+            <div className='gap-patch'>
+              <div className='circle'></div>
             </div>
-            <div className="circle-clipper right">
-              <div className="circle"></div>
+            <div className='circle-clipper right'>
+              <div className='circle'></div>
             </div>
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
